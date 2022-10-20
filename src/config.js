9349ca8857f5e4d930b0dbb7784df561b9bdbe02
N0Ball <n0ball@gmail.com>
2022-10-20 23:47:18 +0800

    ADD: config for test env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    port: 3000,
-    db: {
+    PORT: 3000,
+    DB: {
       production: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/musika`,
       development: `mongodb://mongo:27017/${process.env.MONGO_DB}`,
       test: `mongodb://test-mongo:27017/${process.env.MONGO_DB}`,
--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const config = require("../config")
 
 const connectDB = async() => {
     try{
 
-        const con = await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`);
+        const con = await mongoose.connect(config.DB);
         console.log(`MongoDB connected : ${con.connection.host}`);
 
     }catch(e){
